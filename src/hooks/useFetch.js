f867cb419cc1cef7c42499353426cfd3683678f4
hrithikapps <hrithikk2765@gmail.com>
2025-05-06 04:33:22 +0530

    feat: custom hook for fetchingg data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+// src/hooks/useFetch.js
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("Failed to fetch");
+        const json = await res.json();
+        if (isMounted) setData(json);
+      } catch (err) {
+        if (isMounted) setError(err.message);
+      } finally {
+        if (isMounted) setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
