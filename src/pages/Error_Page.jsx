63acdc10d6104ab4059e6efe854d294f0324110f
hrithikapps <hrithikk2765@gmail.com>
2025-05-06 02:33:06 +0530

    feat: Comopnent / Page structure

---
+++ src/pages/Error_Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error_Page = () => {
+  return (
+    <div>Error_Page</div>
+  )
+}
+
+export default Error_Page
---
+++ src/pages/Exchange_Rates.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Exchange_Rates = () => {
+  return <div>Exchange_Rates</div>;
+};
+
+export default Exchange_Rates;
